b38766c0143176ada31b1a78a18758ccff539be3
tejas sagar <tejas.m.sagar@gmail.com>
2024-01-22 14:31:31 +0000

    ruststuff

---
+++ projects/hello_cargo/target/debug/deps/hello_cargo-4376c0aedfff852b.d
@@ -0,0 +1,5 @@
+/workspace/newrustprojects/projects/hello_cargo/target/debug/deps/hello_cargo-4376c0aedfff852b: src/main.rs
+
+/workspace/newrustprojects/projects/hello_cargo/target/debug/deps/hello_cargo-4376c0aedfff852b.d: src/main.rs
+
+src/main.rs:
---
+++ projects/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/workspace/newrustprojects/projects/hello_cargo/target/debug/hello_cargo: /workspace/newrustprojects/projects/hello_cargo/src/main.rs
